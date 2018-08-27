d399b22917175149c2d9cb56f0443ce9c43a1685
austint <austint@ndev.coic.mil>
2018-08-27 17:21:46 -0400

    section 9  if statements

--- section9.cpp
+++ section9.cpp
@@ -10,6 +10,30 @@ int main()
     high_resolution_clock::time_point start = high_resolution_clock::now();
     cout << endl;
 
+    int num{};
+    const int min{10};
+    const int max{100};
+
+    cout << "Enter a number between " << min << " and " << max << ": ";
+    cin >> num;
+
+    if (num > 10)
+    {
+        cout << "\n============= If statement 1 =============" << endl;
+        cout << num << " is greater than " << min << endl;
+
+        int diff{num - min};
+        cout << num << " is " << diff << " greater than " << min << endl;
+    }
+
+    if (num <= max)
+    {
+        cout << "\n============= If statement 2 =============" << endl;
+        cout << num << " is less than or equal to " << max << endl;
+
+        int diff{max - num};
+        cout << num << " is " << diff << " less than " << max << endl;
+    }
 
     // Clock
     high_resolution_clock::time_point stop = high_resolution_clock::now();
